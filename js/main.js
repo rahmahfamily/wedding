6e78fff3f06b3b2d3e2e4737995f881ce1a1f7fe
Rakhmatullah Yoga Sutrisna <rakhmatullahyoga@gmail.com>
2022-03-23 21:10:28 +0700

    tidying script and load testimony

--- js/main.js
+++ js/main.js
@@ -96,21 +96,6 @@
 	};
 
 
-	var testimonialCarousel = function(){
-		var owl = $('.owl-carousel-fullwidth');
-		owl.owlCarousel({
-			items: 1,
-			loop: true,
-			margin: 0,
-			responsiveClass: true,
-			nav: true,
-			dots: false,
-			smartSpeed: 800,
-			autoHeight: false,
-		});
-	};
-
-
 	var goToTop = function() {
 
 		$('.js-gotop').on('click', function(event){
@@ -168,6 +153,110 @@
 		$(window).stellar();
 	};
 
+	var countdown = function() {
+		// var d = new Date("2022-05-28T08:00:00+07:00");
+
+		var d = new Date(Date.now() + 5000);
+
+		// default example
+		simplyCountdown('.simply-countdown-one', {
+				year: d.getFullYear(),
+				month: d.getMonth() + 1,
+				day: d.getDate(),
+				hours: d.getHours(),
+				minutes: d.getMinutes(),
+				seconds: d.getSeconds(),
+				enableUtc: false,
+				onEnd: function() {
+					var countdown = document.getElementById('countdown');
+					var livestream = document.getElementById('livestream');
+					countdown.style.display = 'none';
+					livestream.style.display = 'block';
+				}
+		});
+	};
+
+	var formSubmission = function() {
+		var responseContainer = document.getElementById('responseContainer');
+		var formContainer = document.getElementById('formContainer');
+		window.addEventListener("load", function() {
+			const form = document.getElementById('rsvp');
+			form.addEventListener("submit", function(e) {
+				e.preventDefault();
+				const data = new FormData(form);
+				const action = e.target.action;
+				fetch(action, {
+					method: 'POST',
+					body: data,
+				})
+				.then(() => {
+					formContainer.style.display = 'none';
+					responseContainer.style.display = 'block';
+				})
+			});
+		});
+	};
+
+	var collapsible = function() {
+		var coll = document.getElementsByClassName("collapsible");
+
+		for (var i = 0; i < coll.length; i++) {
+			coll[i].addEventListener("click", function() {
+				this.classList.toggle("active");
+				var content = this.nextElementSibling;
+				if (content.style.maxHeight){
+					content.style.maxHeight = null;
+				} else {
+					content.style.maxHeight = content.scrollHeight + "px";
+				} 
+			});
+		}
+	};
+
+	var ajax = function() {
+		$.ajax({
+			url: 'https://script.googleusercontent.com/macros/echo?user_content_key=iqxtafa-zg1W3vgqCjXybT0yYM5XWe6aa3h4dctBRspqc1_tM4R-tJQdYzAczm2rOZ-p5C0-73MdXsFkLjq6RxmFmJVO4BYbm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEoSH7AJcn_l1C21T8iYsP8hyhLrHaTSMCc2UvhHBIp7cd8cX-QXImwgL_jim2vnXBQzEB9xT-bKHKATw3r0EfT2DW5H20kYXtz9Jw9Md8uu&lib=M7vB5U2vXQ79BkITwHhiH3fz8cqXX5ADs',
+			dataType: 'json',
+			success: function(data) {
+				if (data.data.length > 0) {
+					var testi = document.getElementById('fh5co-testimonial')
+					testi.style.display = 'block';
+					for (var i in data.data) {
+						var item, subItem, people, testi, paragraph;
+						var owlDiv = document.getElementById('owl-testimony')
+						item = document.createElement('div');
+						item.classList.add('item');
+						subItem = document.createElement('div');
+						subItem.classList.add('testimony-slide')
+						subItem.classList.add('active')
+						subItem.classList.add('text-center')
+						people = document.createElement('span');
+						testi = document.createElement('blockquote');
+						paragraph = document.createElement('p');
+						people.textContent = data.data[i].name;
+						paragraph.textContent = data.data[i].testimony;
+						testi.appendChild(paragraph);
+						subItem.appendChild(people);
+						subItem.appendChild(testi);
+						item.appendChild(subItem);
+						owlDiv.appendChild(item);
+					}
+					if(data.data.length > 1) {
+						$('.owl-carousel-fullwidth').owlCarousel({
+							items: 1,
+							loop: true,
+							margin: 0,
+							responsiveClass: true,
+							nav: true,
+							dots: false,
+							smartSpeed: 800,
+							autoHeight: false,
+						});
+					}
+				}
+			}
+		});
+	};
 	
 	$(function(){
 		mobileMenuOutsideClick();
@@ -175,11 +264,14 @@
 		burgerMenu();
 		contentWayPoint();
 		dropdown();
-		testimonialCarousel();
 		goToTop();
 		loaderPage();
 		counter();
 		counterWayPoint();
+		countdown();
+		formSubmission();
+		collapsible();
+		ajax();
 	});
 
 
